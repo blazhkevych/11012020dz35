4ff994744fe2a481418ab366171c033737f176be
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:47:01 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz35.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры размер в миллиметрах.Перевести его в футы.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите размер в миллиметрах : \n";
+	int a;
+	cin >> a;
+	cout << "футов = " << a * 0.003281 << endl;
+	return 0;
+}
